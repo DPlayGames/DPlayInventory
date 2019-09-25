56e228f1dbcb99eda35eda51966b7b0ea579b521
Hanul <hanul@hanul.me>
2019-09-25 13:24:03 +0900

    In Page 코드 추가

--- DPlayInventory/INPAGE/DPlayInventory.js
+++ DPlayInventory/INPAGE/DPlayInventory.js
@@ -1,4 +1,144 @@
-window.DPlayInventory = () => {
+window.DPlayInventory = (() => {
+	
+	let TO_DELETE = null;
+	
+	let SHOW_ERROR = (tag, errorMsg, params) => {
+		//REQUIRED: tag
+		//REQUIRED: errorMsg
+		//OPTIONAL: params
+		
+		let cal = CALENDAR();
+			
+		console.error(cal.getYear() + '-' + cal.getMonth(true) + '-' + cal.getDate(true) + ' ' + cal.getHour(true) + ':' + cal.getMinute(true) + ':' + cal.getSecond(true) + ' [' + tag + '] 오류가 발생했습니다. 오류 메시지: ' + errorMsg);
+		
+		if (params !== undefined) {
+			console.error('다음은 오류를 발생시킨 파라미터입니다.');
+			console.error(JSON.stringify(params, TO_DELETE, 4));
+		}
+	};
+	
+	let CHECK_IS_DATA = (target) => {
+		//OPTIONAL: target
+
+		if (
+		target !== undefined &&
+		target !== TO_DELETE &&
+		CHECK_IS_ARRAY(target) !== true &&
+		target instanceof Date !== true &&
+		target instanceof RegExp !== true &&
+		typeof target === 'object') {
+			return true;
+		}
+
+		return false;
+	};
+	
+	let CHECK_IS_ARRAY = (target) => {
+		//OPTIONAL: target
+
+		if (
+		target !== undefined &&
+		target !== TO_DELETE &&
+		typeof target === 'object' &&
+		Object.prototype.toString.call(target) === '[object Array]') {
+			return true;
+		}
+
+		return false;
+	};
+	
+	let EACH = (dataOrArrayOrString, func) => {
+		//OPTIONAL: dataOrArrayOrString
+		//REQUIRED: func
+		
+		if (dataOrArrayOrString === undefined) {
+			return false;
+		}
+
+		// when dataOrArrayOrString is data
+		else if (CHECK_IS_DATA(dataOrArrayOrString) === true) {
+
+			for (let name in dataOrArrayOrString) {
+				if (dataOrArrayOrString.hasOwnProperty === undefined || dataOrArrayOrString.hasOwnProperty(name) === true) {
+					if (func(dataOrArrayOrString[name], name) === false) {
+						return false;
+					}
+				}
+			}
+		}
+
+		// when dataOrArrayOrString is func
+		else if (func === undefined) {
+
+			func = dataOrArrayOrString;
+			dataOrArrayOrString = undefined;
+
+			return (dataOrArrayOrString) => {
+				return EACH(dataOrArrayOrString, func);
+			};
+		}
+
+		// when dataOrArrayOrString is array or string
+		else {
+
+			let length = dataOrArrayOrString.length;
+
+			for (let i = 0; i < length; i += 1) {
+
+				if (func(dataOrArrayOrString[i], i) === false) {
+					return false;
+				}
+
+				// when shrink
+				if (dataOrArrayOrString.length < length) {
+					i -= length - dataOrArrayOrString.length;
+					length -= length - dataOrArrayOrString.length;
+				}
+
+				// when stretch
+				else if (dataOrArrayOrString.length > length) {
+					length += dataOrArrayOrString.length - length;
+				}
+			}
+		}
+
+		return true;
+	};
+	
+	let STRINGIFY = (data) => {
+		//REQUIRED: data
+		
+		if (CHECK_IS_DATA(data) === true) {
+			return JSON.stringify(PACK_DATA(data));
+		}
+		
+		else if (CHECK_IS_ARRAY(data) === true) {
+			
+			let f = (array) => {
+				
+				let newArray = [];
+				
+				EACH(array, (data) => {
+					if (CHECK_IS_DATA(data) === true) {
+						newArray.push(PACK_DATA(data));
+					} else if (CHECK_IS_ARRAY(data) === true) {
+						newArray.push(f(data));
+					} else {
+						newArray.push(data);
+					}
+				});
+				
+				return newArray;
+			};
+			
+			return JSON.stringify(f(data));
+		}
+		
+		else {
+			return JSON.stringify(data);
+		}
+	};
+	
 	let self = Connector();
 	
 	let methodMap = {};
@@ -127,4 +267,4 @@ window.DPlayInventory = () => {
 	};
 	
 	return self;
-};
+})();
