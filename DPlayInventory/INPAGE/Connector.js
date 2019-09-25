6b8b3988ee854ac1d9755c8acc62a614616a396c
Hanul <hanul@hanul.me>
2019-09-25 15:21:10 +0900

    Connector 기능 추가

--- DPlayInventory/INPAGE/Connector.js
+++ DPlayInventory/INPAGE/Connector.js
@@ -1,6 +1,98 @@
 // Content Script와 연결되는 커넥터
-window.Connector = () => {
+window.Connector = (pack) => {
+	//REQUIRED: pack
+	
 	let self = {};
+	
+	let methodMap = {};
+	let sendKey = 0;
+	
+	let on = (methodName, method) => {
+		
+		let realMethodName = pack = '/' + methodName;
+	
+		let methods = methodMap[realMethodName];
+	
+		if (methods === undefined) {
+			methods = methodMap[realMethodName] = [];
+		}
+	
+		methods.push(method);
+	};
+	
+	let off = (methodName, method) => {
+		
+		let realMethodName = pack + '/' + methodName;
+		
+		let methods = methodMap[realMethodName];
+	
+		if (methods !== undefined) {
+	
+			if (method !== undefined) {
+				
+				let index = methods.indexOf(method);
+				if (index !== -1) {
+					methods.splice(index, 1);
+				}
+	
+			} else {
+				delete methodMap[realMethodName];
+			}
+		}
+	};
+	
+	let send = (methodName, data, callback) => {
+		
+		let realMethodName = pack + '/' + methodName;
+		
+		let callbackName;
+		
+		window.postMessage({
+			methodName : realMethodName,
+			data : data,
+			sendKey : sendKey
+		}, '*');
+		
+		if (callback !== undefined) {
+			
+			callbackName = '__CALLBACK_' + sendKey;
+	
+			// on callback.
+			on(callbackName, (data) => {
+	
+				// run callback.
+				callback(data);
+	
+				// off callback.
+				off(callbackName);
+			});
+		}
+	
+		sendKey += 1;
+	};
+	
+	window.addEventListener('message', (e) => {
+		if (e.source === window) {
+			
+			let methodName = e.data.methodName;
+			let data = e.data.data;
+			let sendKey = e.data.sendKey;
+			
+			let methods = methodMap[methodName];
+			
+			if (methods !== undefined) {
+				methods.forEach((method) => {
+					method(data, (retData) => {
+						
+						send({
+							methodName : '__CALLBACK_' + sendKey,
+							data : retData
+						});
+					});
+				});
+			}
+		}
+	}, false);
 	
 	return self;
 };
--- DPlayInventory/INPAGE/DPlayInventory.js
+++ DPlayInventory/INPAGE/DPlayInventory.js
@@ -139,97 +139,22 @@ window.DPlayInventory = (() => {
 		}
 	};
 	
-	let self = Connector();
+	let self = Connector('DPlayInventory');
 	
-	let methodMap = {};
-	let sendKey = 0;
-	
-	let on = (methodName, method) => {
+	// 이더리움 네트워크 이름을 가져옵니다.
+	let getNetworkName = self.getNetworkName = (callback) => {
+		//REQUIRED: callback
 		
-		let realMethodName = 'DPlayInventory/' + methodName;
-	
-		let methods = methodMap[realMethodName];
-	
-		if (methods === undefined) {
-			methods = methodMap[realMethodName] = [];
-		}
-	
-		methods.push(method);
+		//TODO:
 	};
 	
-	let off = (methodName, method) => {
+	// 이더리움 네트워크를 변경합니다.
+	let changeNetwork = self.changeNetwork = (networkName, callback) => {
+		//REQUIRED: networkName
+		//REQUIRED: callback
 		
-		let realMethodName = 'DPlayInventory/' + methodName;
-		
-		let methods = methodMap[realMethodName];
-	
-		if (methods !== undefined) {
-	
-			if (method !== undefined) {
-				
-				let index = methods.indexOf(method);
-				if (index !== -1) {
-					methods.splice(index, 1);
-				}
-	
-			} else {
-				delete methodMap[realMethodName];
-			}
-		}
+		//TODO:
 	};
-	
-	let send = (methodName, data, callback) => {
-		
-		let realMethodName = 'DPlayInventory/' + methodName;
-		
-		let callbackName;
-		
-		window.postMessage({
-			methodName : realMethodName,
-			data : data,
-			sendKey : sendKey
-		}, '*');
-		
-		if (callback !== undefined) {
-			
-			callbackName = '__CALLBACK_' + sendKey;
-	
-			// on callback.
-			on(callbackName, (data) => {
-	
-				// run callback.
-				callback(data);
-	
-				// off callback.
-				off(callbackName);
-			});
-		}
-	
-		sendKey += 1;
-	};
-	
-	window.addEventListener('message', (e) => {
-		if (e.source === window) {
-			
-			let methodName = e.data.methodName;
-			let data = e.data.data;
-			let sendKey = e.data.sendKey;
-			
-			let methods = methodMap[methodName];
-			
-			if (methods !== undefined) {
-				methods.forEach((method) => {
-					method(data, (retData) => {
-						
-						send({
-							methodName : '__CALLBACK_' + sendKey,
-							data : retData
-						});
-					});
-				});
-			}
-		}
-	}, false);
 	
 	// 보관함에 로그인합니다.
 	let login = self.login = () => {
