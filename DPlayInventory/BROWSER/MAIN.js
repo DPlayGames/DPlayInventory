7995bd4d05ee080bcfa79272ed36c6064d56acc9
Hanul <hanul@hanul.me>
2019-09-26 20:45:46 +0900

    문서 추가

--- DPlayInventory/BROWSER/MAIN.js
+++ DPlayInventory/BROWSER/MAIN.js
@@ -2,81 +2,84 @@ DPlayInventory.MAIN = METHOD({
 
 	run : () => {
 		
-		DPlayInventory.MATCH_VIEW({
-            uri : 'restoreaccount',
-            target : DPlayInventory.RestoreAccount
-        });
-		
-		DPlayInventory.MATCH_VIEW({
-            uri : 'createaccount',
-            target : DPlayInventory.CreateAccount
-        });
-		
-		DPlayInventory.MATCH_VIEW({
-            uri : 'login',
-            target : DPlayInventory.Login
-        });
-		
-		DPlayInventory.MATCH_VIEW({
-            uri : '**',
-            excludeURI : ['', 'restoreaccount', 'createaccount', 'login'],
-            target : DPlayInventory.CheckData
-        });
-		
-		DPlayInventory.MATCH_VIEW({
-			uri : '**',
-            excludeURI : ['restoreaccount', 'createaccount', 'login'],
-			target : DPlayInventory.Layout
-		});
-		
-		DPlayInventory.MATCH_VIEW({
-			uri : '',
-			target : DPlayInventory.Home
-		});
-		
-		DPlayInventory.MATCH_VIEW({
-			uri : 'account',
-			target : DPlayInventory.Account
-		});
-		
-		DPlayInventory.MATCH_VIEW({
-			uri : 'updateaccount',
-			target : DPlayInventory.UpdateAccount
-		});
-		
-		DPlayInventory.MATCH_VIEW({
-			uri : 'createguild',
-			target : DPlayInventory.CreateGuild
-		});
-		
-		DPlayInventory.MATCH_VIEW({
-			uri : 'updateguild',
-			target : DPlayInventory.UpdateGuild
-		});
-		
-		DPlayInventory.MATCH_VIEW({
-			uri : 'changenetwork',
-			target : DPlayInventory.ChangeNetwork
-		});
-		
-		DPlayInventory.MATCH_VIEW({
-			uri : 'guild',
-			target : DPlayInventory.Guild
-		});
-		
-		DPlayInventory.MATCH_VIEW({
-			uri : 'game',
-			target : DPlayInventory.Game
-		});
-		
-		DPlayInventory.MATCH_VIEW({
-			uri : 'money',
-			target : DPlayInventory.Money
-		});
-		
-		DPlayInventory.MATCH_VIEW({
-			uri : 'item',
-			target : DPlayInventory.Item
+		MSG.loadCSV(DPlayInventory.R('text.csv'), () => {
+			
+			DPlayInventory.MATCH_VIEW({
+	            uri : 'restoreaccount',
+	            target : DPlayInventory.RestoreAccount
+	        });
+			
+			DPlayInventory.MATCH_VIEW({
+	            uri : 'createaccount',
+	            target : DPlayInventory.CreateAccount
+	        });
+			
+			DPlayInventory.MATCH_VIEW({
+	            uri : 'login',
+	            target : DPlayInventory.Login
+	        });
+			
+			DPlayInventory.MATCH_VIEW({
+	            uri : '**',
+	            excludeURI : ['', 'restoreaccount', 'createaccount', 'login'],
+	            target : DPlayInventory.CheckData
+	        });
+			
+			DPlayInventory.MATCH_VIEW({
+				uri : '**',
+	            excludeURI : ['restoreaccount', 'createaccount', 'login'],
+				target : DPlayInventory.Layout
+			});
+			
+			DPlayInventory.MATCH_VIEW({
+				uri : '',
+				target : DPlayInventory.Home
+			});
+			
+			DPlayInventory.MATCH_VIEW({
+				uri : 'account',
+				target : DPlayInventory.Account
+			});
+			
+			DPlayInventory.MATCH_VIEW({
+				uri : 'updateaccount',
+				target : DPlayInventory.UpdateAccount
+			});
+			
+			DPlayInventory.MATCH_VIEW({
+				uri : 'createguild',
+				target : DPlayInventory.CreateGuild
+			});
+			
+			DPlayInventory.MATCH_VIEW({
+				uri : 'updateguild',
+				target : DPlayInventory.UpdateGuild
+			});
+			
+			DPlayInventory.MATCH_VIEW({
+				uri : 'changenetwork',
+				target : DPlayInventory.ChangeNetwork
+			});
+			
+			DPlayInventory.MATCH_VIEW({
+				uri : 'guild',
+				target : DPlayInventory.Guild
+			});
+			
+			DPlayInventory.MATCH_VIEW({
+				uri : 'game',
+				target : DPlayInventory.Game
+			});
+			
+			DPlayInventory.MATCH_VIEW({
+				uri : 'money',
+				target : DPlayInventory.Money
+			});
+			
+			DPlayInventory.MATCH_VIEW({
+				uri : 'item',
+				target : DPlayInventory.Item
+			});
 		});
 	}
 });
